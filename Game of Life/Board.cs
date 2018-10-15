b38f3149c1501b132352e0204c47c58b1c0b9e93
Tyler Atkinson <tylerh.atkinson@gmail.com>
2018-10-15 16:03:39 -0400

    Moved things out of GameOfLife and into board

---
+++ Game of Life/Board.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Game_of_Life {
+    public class Board {
+        private int iteration = 0;
+        private Texture2D whiteSquare, blackSquare;
+        private readonly Random random = new Random();
+        private int squareSize = 10;
+        private bool[][] lastTick;
+
+        public bool[][] Squares;
+        public int Width, Height;
+        public bool IsPaused = true;
+
+        public Board(GraphicsDevice device) {
+            //White texture
+            whiteSquare = new Texture2D(device, squareSize, squareSize);
+            Color[] data = new Color[squareSize * squareSize];
+            for (int i = 0; i < data.Length; i++) {
+                data[i] = Color.White;
+            }
+            whiteSquare.SetData(data);
+            //Black texture
+            blackSquare = new Texture2D(device, squareSize, squareSize);
+            data = new Color[squareSize * squareSize];
+            for (int i = 0; i < data.Length; i++) {
+                data[i] = Color.Black;
+            }
+            blackSquare.SetData(data);
+            //Finding size of the matrix
+            Height = device.Viewport.Height / squareSize;
+            Width = device.Viewport.Width / squareSize;
+            //Initializing the matrices
+            Squares = new bool[device.Viewport.Height / squareSize][];
+            lastTick = new bool[device.Viewport.Height / squareSize][];
+            for (int i = 0; i < Squares.Length; i++) {
+                Squares[i] = new bool[Width];
+                lastTick[i] = new bool[Width];
+                for (int j = 0; j < Width; j++) {
+                    lastTick[i][j] = false;
+                    Squares[i][j] = false;
+                }
+            }
+        }
+        public void Update(Viewport viewport) {
+            if (Keyboard.GetState().IsKeyDown(Keys.Space)) {
+                IsPaused = true;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Enter))
+                IsPaused = false;
+            HandleClick(viewport);
+            if (!IsPaused && iteration++ == 3) {
+                CalculateNextTick();
+                iteration = 0;
+            }
+        }
+        private void CalculateNextTick() {
+            for (int i = 0; i < Squares.Length; i++) {
+                for (int j = 0; j < Squares[0].Length; j++) {
+                    int number = GetNumberOfNeighbors(j, i);
+                    if (lastTick[i][j]) {
+                        if (number < 2)
+                            Squares[i][j] = false;
+                        else if (number > 3)
+                            Squares[i][j] = false;
+                    }
+                    else if (number == 3)
+                        Squares[i][j] = true;
+                }
+            }
+            for (int i = 0; i < Height; i++)
+                Squares[i].CopyTo(lastTick[i], 0);
+        }
+        private int GetNumberOfNeighbors(int x, int y) {
+            int total = 0;
+            if (y < 0 || y > Height)
+                return total;
+            if (x < 0 || x > Width)
+                return total;
+            //Horizontal
+            if (x > 1) {
+                if (lastTick[y][x - 1])
+                    total++;
+            }
+            if (x < Width - 1) {
+                if (lastTick[y][x + 1])
+                    total++;
+            }
+            //Vertical
+            if (y > 1) {
+                if (lastTick[y - 1][x])
+                    total++;
+            }
+            if (y < Height - 1) {
+                if (lastTick[y + 1][x])
+                    total++;
+            }
+            //Diagonal
+            if (x > 1) {
+                if (y > 1) {
+                    if (lastTick[y - 1][x - 1])
+                        total++;
+                }
+                if (y < Height - 1) {
+                    if (lastTick[y + 1][x - 1])
+                        total++;
+                }
+            }
+            if (x < Width - 1) {
+                if (y > 1) {
+                    if (lastTick[y - 1][x + 1])
+                        total++;
+                }
+                if (y < Height - 1) {
+                    if (lastTick[y + 1][x + 1])
+                        total++;
+                }
+            }
+            return total;
+        }
+        private bool IsMouseInWindow(Viewport viewport) {
+            Point pos = new Point(Mouse.GetState().X, Mouse.GetState().Y);
+            return viewport.Bounds.Contains(pos);
+        }
+        public void HandleClick(Viewport viewport) {
+            MouseState state = Mouse.GetState();
+            if (!IsMouseInWindow(viewport))
+                return;
+            int x = state.X / squareSize;
+            int y = state.Y / squareSize;
+            if (state.LeftButton == ButtonState.Pressed) {
+                lastTick[y][x] = true;
+                Squares[y][x] = true;
+            }
+            if (state.RightButton == ButtonState.Pressed) {
+                lastTick[y][x] = false;
+                Squares[y][x] = false;
+            }
+        }
+        public void Save(string fileName) {
+            string[] lines = new string[Height + 1];
+            lines[0] = string.Format("#squareSize: {0}, #width: {1}, #height: {2}", squareSize, Width, Height);
+            int fileIndex = 1;
+            for (int i = 0; i < Height; i++) {
+                string line = "";
+                for (int j = 0; j < Width; j++) {
+                    line += Squares[i][j] + ", ";
+                }
+                lines[fileIndex] = line;
+            }
+            System.IO.File.WriteAllLines(fileName, lines);
+        }
+        public void Draw(GameTime gameTime, GraphicsDevice device, SpriteBatch spriteBatch) {
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < Squares.Length; i++) {
+                for (int j = 0; j < Squares[0].Length; j++) {
+                    Vector2 position = new Vector2(x, y);
+                    if (Squares[i][j]) {
+                        spriteBatch.Draw(whiteSquare, position, Color.White);
+                    }
+                    else {
+                        spriteBatch.Draw(blackSquare, position, Color.Black);
+                    }
+                    x += squareSize;
+                }
+                x = 0;
+                y += squareSize;
+            }
+        }
+    }
+}
--- Game of Life/GameOfLife.cs
+++ Game of Life/GameOfLife.cs
@@ -2,7 +2,6 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
-
 namespace Game_of_Life {
     /// <summary>
     /// This is the main type for your game.
@@ -10,20 +9,15 @@ namespace Game_of_Life {
     public class GameOfLife : Game {
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
-        private int iteration = 0;
-        private Texture2D whiteSquare, blackSquare;
-        private readonly Random random = new Random();
-        private int squareSize = 10;
-        private bool[][] lastTick;
-
-        public bool[][] Squares;
-        public int Width, Height;
-        public bool IsPaused = true;
+        private Board board;
 
         public GameOfLife() {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.PreferredBackBufferWidth = 800;
+            Window.AllowUserResizing = false;
         }
 
         /// <summary>
@@ -33,32 +27,7 @@ namespace Game_of_Life {
         /// and initialize them as well.
         /// </summary>
         protected override void Initialize() {
-            // TODO: Add your initialization logic here
-            whiteSquare = new Texture2D(graphics.GraphicsDevice, squareSize, squareSize);
-            Color[] data = new Color[squareSize * squareSize];
-            for (int i = 0; i < data.Length; i++) {
-                data[i] = Color.White;
-            }
-            whiteSquare.SetData(data);
-
-            blackSquare = new Texture2D(graphics.GraphicsDevice, squareSize, squareSize);
-            data = new Color[squareSize * squareSize];
-            for (int i = 0; i < data.Length; i++) {
-                data[i] = Color.Black;
-            }
-            blackSquare.SetData(data);
-            Height = graphics.GraphicsDevice.Viewport.Height / squareSize;
-            Width = graphics.GraphicsDevice.Viewport.Width / squareSize;
-            Squares = new bool[graphics.GraphicsDevice.Viewport.Height / squareSize][];
-            lastTick = new bool[graphics.GraphicsDevice.Viewport.Height / squareSize][];
-            
-            for (int i = 0; i < Squares.Length; i++) {
-                Squares[i] = new bool[Width];
-                lastTick[i] = new bool[Width];
-                for(int j = 0; j < Width; j++) {
-                    lastTick[i][j] = false;
-                }
-            }
+            board = new Board(GraphicsDevice);
             base.Initialize();
         }
 
@@ -89,102 +58,10 @@ namespace Game_of_Life {
         protected override void Update(GameTime gameTime) {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            if (Keyboard.GetState().IsKeyDown(Keys.Space)) {
-                IsPaused = true;    
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.Enter))
-                    IsPaused = false;
-            HandleClick();
-            if (!IsPaused && iteration++ == 3) {
-                CalculateNextTick();
-                iteration = 0;
-            }
+            board.Update(GraphicsDevice.Viewport);
             base.Update(gameTime);
         }
-        private void CalculateNextTick() {
-            for(int i = 0; i < Squares.Length; i++) {
-                for(int j = 0; j < Squares[0].Length; j++) {
-                    int number = GetNumberOfNeighbors(j, i);
-                    if (lastTick[i][j]) {
-                        if (number < 2)
-                            Squares[i][j] = false;
-                        else if (number > 3)
-                            Squares[i][j] = false;
-                    }
-                    else if (number == 3)
-                        Squares[i][j] = true;
-                }
-            }
-            for(int i = 0; i < Height; i++)
-                Squares[i].CopyTo(lastTick[i], 0);
-        }
-        private int GetNumberOfNeighbors(int x, int y) {
-            int total = 0;
-            if (y < 0 || y > Height)
-                return total;
-            if (x < 0 || x > Width)
-                return total;
-            //Horizontal
-            if(x > 1) {
-                if (lastTick[y][x - 1])
-                    total++;
-            }
-            if(x < Width - 1) {
-                if (lastTick[y][x + 1])
-                    total++;
-            }
-            //Vertical
-            if(y > 1) {
-                if (lastTick[y - 1][x])
-                    total++;
-            }
-            if(y < Height - 1) {
-                if (lastTick[y + 1][x])
-                    total++;
-            }
-            //Diagonal
-            if(x > 1) {
-                if(y > 1) {
-                    if (lastTick[y - 1][x - 1])
-                        total++;
-                }
-                if(y < Height - 1) {
-                    if (lastTick[y + 1][x - 1])
-                        total++;
-                }
-            }
-            if(x < Width - 1) {
-                if(y > 1) {
-                    if (lastTick[y - 1][x + 1])
-                        total++;
-                }
-                if(y < Height - 1) {
-                    if (lastTick[y + 1][x + 1])
-                        total++;
-                }
-            }
-            return total;
-        }
-        private bool IsMouseInWindow() {
-            Point pos = new Point(Mouse.GetState().X, Mouse.GetState().Y);
-            return GraphicsDevice.Viewport.Bounds.Contains(pos);
-        }
-        private void HandleClick() {
-            MouseState state = Mouse.GetState();
-            if (!IsMouseInWindow())
-                return;
-            int x = state.X / squareSize;
-            int y = state.Y / squareSize;
-            if (state.LeftButton == ButtonState.Pressed) {
-                lastTick[y][x] = true;
-                Squares[y][x] = true;
-            }
-            if (state.RightButton == ButtonState.Pressed) {
-                lastTick[y][x] = false;
-                Squares[y][x] = false;
-            }
 
-        }
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
@@ -192,23 +69,9 @@ namespace Game_of_Life {
         protected override void Draw(GameTime gameTime) {
             GraphicsDevice.Clear(Color.Bisque);
             spriteBatch.Begin();
-            int x = 0;
-            int y = 0;
-            for(int i = 0; i < Squares.Length; i++) {
-                for(int j = 0; j < Squares[0].Length; j++) {
-                    Vector2 position = new Vector2(x, y);
-                    if (Squares[i][j]) {
-                        spriteBatch.Draw(whiteSquare, position, Color.White);
-                    }
-                    else {
-                        spriteBatch.Draw(blackSquare, position, Color.Black);
-                    }
-                    x += squareSize;
-                }
-                x = 0;
-                y += squareSize;
-            }
 
+            board.Draw(gameTime, GraphicsDevice, spriteBatch);
+            
             spriteBatch.End();
             base.Draw(gameTime);
         }
